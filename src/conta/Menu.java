771cd06deeda13c2ded656b3beeb0370685f612d
mariliatostesdev <mahtostes.dev@gmail.com>
2023-12-14 16:03:12 -0300

    additions to Menu class

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,7 +1,7 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.ContaBancaria;
 import conta.util.Cores;
 
 public class Menu {
@@ -43,10 +43,36 @@ public class Menu {
 				System.exit(0);
 			}
 
+			String tipoNovaConta;
+			
 			switch (opcao) {
 			case 1:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar conta\n\n");
-
+				ContaBancaria novaConta = new ContaBancaria(32157, 7697, 0, null, 0);
+				System.out.println("\nPreencha os dados a seguir para criar sua conta:");
+				System.out.println("\nDeseja criar uma conta (1)-Corrente  ou (2)-Poupança?");
+				novaConta.setTipo(input.nextInt());;
+				input.nextLine();
+				System.out.println("\nQual o nome do titular da conta?");
+				novaConta.setTitular(input.nextLine());
+				System.out.println("\nQual valor deseja depositar como seu saldo inicial?");
+				novaConta.setSaldo(input.nextFloat());
+				System.out.println("\n\n\tConta criada com sucesso! Os dados da sua conta são:");
+				System.out.println("\n\tNúmero da conta: " + novaConta.getNumero());
+				System.out.println("\n\tNúmero da agência: " + novaConta.getAgencia());
+				
+				if (novaConta.getTipo() == 1) {
+					tipoNovaConta = "Conta Corrente";
+				} else {
+					tipoNovaConta = "Conta Poupança";
+				}
+				
+				System.out.println("\n\tTipo de conta: " + tipoNovaConta);
+				System.out.println("\n\tNome do titular da conta: " + novaConta.getTitular());
+				System.out.println("\n\tSaldo da conta: R$" + novaConta.getSaldo());
+				System.out.println("                                               ");
+				System.out.println("***********************************************");
+				
 				break;
 			case 2:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
